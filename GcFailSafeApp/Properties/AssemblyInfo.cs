7fa3d2b579050ecc72cc09269c7c6552a962f9c2
GeorgeSchiro <9727github@schiro.name>
2025-09-27 11:22:52 -0400

    Made sReplaceProfileTokens() reusable.

--- GcFailSafeApp/Properties/AssemblyInfo.cs
+++ GcFailSafeApp/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("GoGetCert.com")]
 [assembly: AssemblyProduct("GcFailSafe")]
-[assembly: AssemblyCopyright("Copyright © GoGetCert.com 2020-2121")]
+[assembly: AssemblyCopyright("Copyright © George Schiro 2020-2121")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -23,5 +23,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.17")]
-[assembly: AssemblyFileVersion("2.17")]
+[assembly: AssemblyVersion("2.18")]
+[assembly: AssemblyFileVersion("2.18")]
