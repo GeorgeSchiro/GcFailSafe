59d58a6fdedbbf8792956c50deeed8831b1f6819
GeorgeSchiro <9727github@schiro.name>
2026-07-11 10:20:56 -0400

    Added support for .Net 6+ profile files.

--- GcFailSafeApp/Properties/AssemblyInfo.cs
+++ GcFailSafeApp/Properties/AssemblyInfo.cs
@@ -23,5 +23,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.194")]
-[assembly: AssemblyFileVersion("2.194")]
+[assembly: AssemblyVersion("2.195")]
+[assembly: AssemblyFileVersion("2.195")]
